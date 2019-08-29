50826256565076b8fae0b1aa05cba4fee76f159f
Dean <dean.juricic@gmail.com>
2019-08-29 22:17:36 +0200

    component nesting examples and props

--- src/index.js
+++ src/index.js
@@ -1,30 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard'
 import faker from 'faker';
 
 const App = () => {
 
 	return (
 		<div className="ui container comments">
-			<CommentDetail 
-				author = "Alex" 
-				timeAgo = "Today at 2:02PM" 
-				text="Dude you missed one comment below XD" 
-				src={faker.image.avatar()}
-			/>
-			<CommentDetail 
-				author = "Sam" 
-				timeAgo = "Today at 4:45AM" 
-				text="I commented on this first!" 
-				src={faker.image.avatar()}
-			/>
-			<CommentDetail 
-				author = "Jane" 
-				timeAgo = "Yesterday at 1:40PM" 
-				text="Great text!" 
-				src={faker.image.avatar()}
-			/>
+			<ApprovalCard>
+				<CommentDetail
+					author="Alex"
+					timeAgo="Today at 2:02PM"
+					text="Dude you missed one comment below XD"
+					src={faker.image.avatar()}
+				/>
+			</ApprovalCard>
+			<ApprovalCard>
+				<CommentDetail
+					author="Sam"
+					timeAgo="Today at 4:45AM"
+					text="I commented on this first!"
+					src={faker.image.avatar()}
+				/>
+			</ApprovalCard>
+			<ApprovalCard>
+				<CommentDetail
+					author="Jane"
+					timeAgo="Yesterday at 1:40PM"
+					text="Great text!"
+					src={faker.image.avatar()}
+				/>
+			</ApprovalCard>
 		</div>
 
 	);
